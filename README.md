3ee08edd69b05451bdc490f3cda962a96aef3715
riyad <riyad>
2017-03-28 15:10:46 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# p1-bourse-societe-service" 
---
+++ src/main/java/com/perfect/P1BourseSocieteServiceApplication.java
@@ -0,0 +1,12 @@
+package com.perfect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1BourseSocieteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1BourseSocieteServiceApplication.class, args);
+	}
+}
